0cfa4b761efae2ad5a3779853951f18493ea1ca2
theDeadInside228 <a320859@gmail.com>
2024-10-13 14:44:07 +0500

    Добавил примеры параметризации

--- something.py
+++ something.py
@@ -1,4 +1,36 @@
 import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+@pytest.mark.parametrize('language', ["ru", "en-gb"]) # Пример параметризации тестов ..............................
+def test_guest_should_see_login_link(browser, language):
+    link = f"http://selenium1py.pythonanywhere.com/{language}/"
+    browser.get(link)
+    browser.find_element(By.CSS_SELECTOR, "#login_link")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""import pytest
 
 @pytest.mark.xfail(strict=True)
 def test_succeed():
@@ -12,7 +44,7 @@ def test_not_succeed():
 
 @pytest.mark.skip
 def test_skipped():
-    assert False
+    assert False"""
 
 
 
